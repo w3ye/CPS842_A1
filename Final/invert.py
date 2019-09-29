78e7f847bca6a438768ab8bbac04a4202ba30a16
William Ye <benwgye@gmail.com>
2019-09-29 00:21:21 -0400

    readDoc

---
+++ Final/invert.py
@@ -0,0 +1,21 @@
+import sys,os,re,time,math
+
+class Invert:
+
+    docHash = {}
+
+    #read all the content in cacm.all
+    def readDoc(self):
+        file = open("./cacm/cacm.all")
+        data = file.read()
+        file.close()
+        return data
+
+    def initDocHash(self):
+        
+        return
+    
+    
+
+
+Invert().readDoc()
