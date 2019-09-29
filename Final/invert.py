c8ddc7daf2ae490eaf50dcf21eaecca001ff4bde
William Ye <benwgye@gmail.com>
2019-09-29 01:07:24 -0400

    initDocHas can now separate docID

--- Final/invert.py
+++ Final/invert.py
@@ -12,6 +12,14 @@ class Invert:
         return data
 
     def initDocHash(self):
+        data = self.readDoc()
+        file = open("./test","a")
+        for i in data.split(".I"):
+            if ".T\n" in i and ".W\n" in i:
+                docID = i.split()[0]
+                print(docID)
+        
+        file.close()
         return
 
-Invert().readDoc()
+Invert().initDocHash()
