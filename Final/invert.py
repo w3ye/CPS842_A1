029e0e76d7f456989a89e8d82472943385206389
Benny Ye <benwgye@gmail.com>
2019-09-30 15:04:49 -0400

    invert.py fix

--- Final/invert.py
+++ Final/invert.py
@@ -19,7 +19,7 @@ class Invert:
             if ".T\n" in i and ".W\n" in i:
                 docID = i.split()[0]
                 self.docHash[docID] = 1
-                file.write(self.appendAbstractTitle(i))
+                self.docHas[docID] = self.appendAbstractTitle(i)
                 self.docNum += 1
 
         file.close()
@@ -33,5 +33,24 @@ class Invert:
             if "\nW\n" in l or "\nT\n" in l:
                 temp += l
         return temp
+    
+    #takes in the data from initDocHash and take out all words individually
+    def initFrequency(self, data):
+        temp = ""
+        temp = data.replace('\n',' ')
+        temp = re.sub('[^A-Za-z]+',' ',temp)
+        temp = temp.split()
+        for i in self.stop:
+            if i in temp:
+                temp.remove(i)
+        return temp
+    
+    def initToken(self):
+        file = open("./test","w")
+        for key,value in self.docHash.items():
+            file.write(value+"\n")
+        file.close()
+        return
+
 
 Invert().initDocHash()
