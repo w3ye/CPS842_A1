23c1d36aa98b64ea0d40824cb04c83ac517fa682
Xiao4Dan <Xiao4Dan@users.noreply.github.com>
2019-10-01 00:24:08 -0400

    updated timer and question

--- Final/test.py
+++ Final/test.py
@@ -1,4 +1,4 @@
-import sys,os,re
+import sys,os,re,time
 
 def test(term)
   return "searching for term related content"
@@ -9,14 +9,32 @@ termNfrequency = dictionary.read()
 
 
 #->
-postingT = open("./postingTerm.txt","r")
+posting = open("./posting","r")
 termNposting = postingT.read()
 
 
-#->
-postingD = open("./postingDoc.txt","r")
-docNtermposition = postingD.read()
+stop = ""
+while stop.lower() != "n" and stop.lower() != "y":
+  stop = input("Would you like to use stopwords?(N/Y)")
+  
+ps = ""
+while ps.lower() != "n" and ps.lower() != "y":
+  ps = input("Would you like to use Porter Stemming Algorithm?(N/Y)")
 
-required = input("What term are you searching for?")
-while required != "ZZEND"
+required = ""
+while required != "ZZEND"：
   required = input("What term are you searching for?")
+  print("Searhing for: "+required)
+  start = time.time()
+
+
+
+
+  '''
+  Put the body of excutional code here
+  '''
+
+  end = time.time()
+  print("The program used: "+ str(end-start)+"seconds.")
+
+print("Program ends.")
