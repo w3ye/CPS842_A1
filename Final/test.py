615649980c459770de96efe941d07c4b87d851f3
Xiao4Dan <Xiao4Dan@users.noreply.github.com>
2019-10-02 16:58:44 -0400

    runnable test with invert
    porter stemming algorithm to be added

--- Final/test.py
+++ Final/test.py
@@ -1,48 +1,45 @@
-import sys,os,re,time
-#if without class you can just def a main function which triggers other functions such as initDoc or initPosting
-from invert import main
-#if you have set up main class, then just simply import the whole document
+import sys,os,math,time
 import invert
 
-def test(term)
-  return "searching for term related content"
-
-#->
+'''
+#->dictionary file as input
 dictionary = open("./dictionary","r")
 termNfrequency = dictionary.read()
 
-
-#->
+#->posting file as input
 posting = open("./posting","r")
 termNposting = posting.read()
 
 posting.close()
 dictionary.close()
+'''
 
 
-#->
-stop = ""
-while stop.lower() != "n" and stop.lower() != "y":
-  stop = input("Would you like to use stopwords?(N/Y)")
-  
+#--------------------------------
+#->asking for user input
+st = ""
+while st.lower() != "n" and st.lower() != "y":
+	st = raw_input("Would you like to use stopwords?(N/Y)")
+
 ps = ""
 while ps.lower() != "n" and ps.lower() != "y":
-  ps = input("Would you like to use Porter Stemming Algorithm?(N/Y)")
+	ps = raw_input("Would you like to use Porter Stemming Algorithm?(N/Y)")
+print("got it")
 
 required = ""
-while required != "ZZEND"：
-  required = input("What term are you searching for?")
-  print("Searhing for: "+required)
-  start = time.time()
+while required != "ZZEND":
+	#the loop will keep running untill the user say ZZEND
+	required = raw_input("What term are you searching for?")
+	print("Searhing for: "+required)
+	start = time.time()
 
+	#----------------------------------------
+	tester = invert.main(ps,st)
+	tester.initDoc()
+	#----------------------------------------
 
+	end = time.time()
+	print("The program used: "+ str(end-start)+"seconds.")
 
-
-  '''
-  Put the body of excutional code here
-  '''
-
-  end = time.time()
-  print("The program used: "+ str(end-start)+"seconds.")
 
 print("Program ends.")
