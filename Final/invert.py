2779a93e095bed48b3c5afad9e9648d9a89cac64
AS <alexshi416@gmail.com>
2019-11-06 01:08:13 -0500

    updated similarity calculation

--- Final/invert.py
+++ Final/invert.py
@@ -5,8 +5,8 @@ from nltk.stem import PorterStemmer
 '''
 New data strcture for dictionary and postings
 dictionary postings
-term | frequency -> docid | frequency | positions
-		 -> docid | frequency | positions
+term -> docid | frequency | positions
+     -> docid | frequency | positions
 
 {
 [t1]:[
@@ -18,16 +18,16 @@ term | frequency -> docid | frequency | positions
 '''
 
 class main:
-	rawdocHash = {}
-	docHash = {}
+	docToken = {}
+	doc = {}
 	tokenHash = {}
-	stopwords = []
-	postingHash = {}
-	weightHash = {}
-	queryList = []
 	queryWeight = {}
+	postingHash = {}
+	docWeight = {}
+	stopwords = []
+
 	stem = ""
-	def __init__(self,algorithm,stop):
+	def __init__(self, algorithm, stop):
 		if stop.lower() == "y": self.getStop()
 		self.stem = algorithm.lower()
 		self.initDoc()
@@ -36,16 +36,19 @@ class main:
 		self.initWeights()
 		self.writeOut()
 
-	def readDoc(self,docName):
-		#helper function for reading document data to avoid duplicte
+	def readDoc(self, docName):
 		file = open(docName,"r")
 		data = file.read()
 		file.close()
 		return data
 
+	def getStop(self):
+		data = self.readDoc("./cacm/common_words")
+		for w in data.split():
+			self.stopwords.append(w)	
+
 	def initDoc(self):
-		'''
-		This functions reads all documents from file
+		'''This functions reads all documents from file
 		Each file is separated by \n.I
 		File content is only validated and stored if it has both abstract and title
 		The content is filtered as a list of tokenized letters
@@ -54,18 +57,12 @@ class main:
 		for f in data.split("\n.I"):
 			if ".T\n" in f and ".W\n" in f:
 				docID = f.split()[0]
-				self.docHash[docID] = self.tokenize(f)
-				self.rawdocHash[docID] = self.filt(f)
+				self.docToken[docID] = self.tokenize(f)
+				self.doc[docID] = self.filt(f)
 	
 
 	def tokenize(self,raw):
-		'''
-		//input the raw content of each book
-		.W
-		blah blahblha
-		.T
-		bla bbb
-		//and return as a list of letters combining both
+		'''Tokenize all the terms in abstract and title
 		'''
 		parts = raw.split("\n.")#split categories like abstracts, titles etc.
 		temp = ""
@@ -78,7 +75,7 @@ class main:
 		return self.clean(temp)
 
 	def filt(self,raw):
-		'''split each part by .^?
+		'''Split out each title abstract and author of the book
 		'''
 		re = {}
 		for block in raw.split("\n."):
@@ -88,45 +85,44 @@ class main:
 		return re
 
 	def clean(self,raw):
-		#the cleaning function that removes all non letters or nextlines
-		raw = raw.replace("\n"," ")#put everything in the same line
-		words = re.sub('[^A-Za-z]+',' ',raw)#change all the non-letters to space
+		'''Removes all non letters and next lines
+		'''
+		raw = str(raw).replace("\n"," ")
+		words = re.sub('[^A-Za-z]+',' ',raw)
 		wordList = [x for x in words.lower().split() if x not in self.stopwords]
-		return wordList
+		ps = PorterStemmer()
+		if self.stem.lower() == "y":
+			for w in wordList:
+				w = ps.stem(w)		
+		return sorted(wordList)
 
 
 	def initToken(self):
+		'''Initializes all unique terms and store in hashmap alphabetically
 		'''
-		This function initilizes tokens
-		storing all unique tokens in a hashmap
-		and storing all raw(valid terms only) docs in a hashmap
-		'''
-		ps = PorterStemmer()
-		for docVal in self.docHash.values():
+		tokens = {}
+		for docVal in self.docToken.values():
 				for term in docVal:
-					if self.stem == "y": term = ps.stem(term)
-					if term in self.tokenHash: self.tokenHash[term]+=1
-					else: self.tokenHash[term]=1
+					if term in tokens: tokens[term]+=1
+					else: tokens[term]=1
+		for token in sorted(tokens.keys()):
+			self.tokenHash[token] = tokens[token]
 
 	def initPosting(self):
-		print("initializing postings")
 		for terms in self.tokenHash:
-			self.postingHash[terms]=self.getPost(terms)
+			self.postingHash[terms] = self.getPost(terms)
 			
 
 	def getPost(self,target):
-		'''
-		search over all valid documents for a certain term
-		returns a list of maps for each term
-
-		return a list of docs
+		'''Search over all valid documents for a certain term
+		returns a list of docs for each term:
 		[
 			[doc1,2,pos1,pos2]
 			[doc2,3,pos1,pos2,pos3]
 		]
 		'''
 		temp = []
-		for docID,docVal in self.docHash.items():
+		for docID,docVal in self.docToken.items():
 			if target in docVal:
 				positions = [str(index+1) for index,value in enumerate(docVal) if target == value]
 				temp.append([docID,len(positions)] + positions)
@@ -136,32 +132,25 @@ class main:
 	def writeOut(self):
 		#write dictionary to file in order of terms
 		dictionary = open("./dictionary","w")
-		for t in sorted(self.tokenHash):
+		for t in self.tokenHash:
 			dictionary.write(t+"&"+str(self.tokenHash[t])+"\n")
-		dictionary.close()
 		#write postings to file in order of terms
 		posting = open("./posting","w")
-		for term in sorted(self.postingHash):
+		for term in self.postingHash:
 			posting.write(str(term) + " : " + str(self.postingHash[term]) + "\n")
-		#write tfs
-		termfrequency = open("./tfs","w")
-		for d,f in self.weightHash.items():
-			termfrequency.write("docID: " + str(d) + " tfs: " + str(f) + "\n")
 		#write raw docs
-		f = open("./docs","w")
-		for i,l in self.docHash.items():
+		f = open("./tokenizedDocs","w")
+		for i,l in self.docToken.items():
 			f.write(i + " :\n"+ str(l) + "\n")
+		#
+		tf = open("./tfs","w")
+		for d,w in self.docWeight.items():
+			tf.write(str(d) + ": " + str(w))
 
 		dictionary.close()
 		posting.close()
-		termfrequency.close()
 		f.close()
-
-	def getStop(self):
-		#read common words into stopwords list when necessary
-		data = self.readDoc("./cacm/common_words")
-		for w in data.split():
-			self.stopwords.append(w)
+		tf.close()
 
 	def getWeight(self,ls1,ls2):
 		'''calculate similarity between two lists: tfs and idfs
@@ -177,63 +166,80 @@ class main:
 		for each term shown in doc
 		'''
 		print("initializing document weights")
-		n = len(self.tokenHash.keys())
+		n = len(self.docToken.keys())
 		#
 		idfs = []
 		for f in self.postingHash.values():
 			if len(f)==0: idfs.append(0)
 			else: idfs.append(math.log(n/len(f)))
 		#
-		for docID, docVal in self.docHash.items():
+		for docID, docVal in self.docToken.items():
 			tfs = []
 			for t,f in self.postingHash.items():
 				if t not in docVal: tfs.append(0)
 				else:
 					for docs in f:
 						if docs[0] == str(docID): tfs.append(1 + math.log(docs[1]))
-			self.weightHash[docID] = self.getWeight(tfs,idfs)
+			self.docWeight[docID] = self.getWeight(tfs,idfs)
 
-	def generateQuery(self,required):
+	def generateQuery(self, required):
 		'''tokenize query to list of terms
 		'''
 		self.queryList = self.clean(required)
-		if self.stem == "y":
-			ps = PorterStemmer()
-			for q in self.queryList:
-				q = ps.stem(q)
+		print(str(self.queryList))
+		queryWeight = {}
 		for t in self.queryList:
-			if t in self.tokenHash and t not in self.queryWeight.keys():
-				self.queryWeight[t]=1
-			elif t in self.tokenHash and t in self.queryWeight.keys():
-				self.queryWeight[t]+=1
-		return self.queryList
+			if t in self.tokenHash and t not in queryWeight.keys():
+				queryWeight[t]=1
+			elif t in self.tokenHash and t in queryWeight.keys():
+				queryWeight[t]+=1
+		return list(queryWeight.values())
 
 	def retrieveQuery(self):
+		'''Read given queries
+		'''
+		defaultQuery = self.readDoc("./cacm/query.text")
+		for part in defaultQuery.split(".I "):
+			for q in part.split("\n."):
+				if "W\n" in q:
+					self.topKSimilarity(self.generateQuery(q.split()[1:]))
+
+	def topKSimilarity(self, queryWeight):
 		'''Assume retrieve top 5 results
 		'''
+		print(str(queryWeight))
 		queryLength = 0.0
-		for x in self.queryWeight.values():
+		for x in queryWeight:
 			queryLength += x**2
 		similarity = []
-		for doc,val in self.docHash.items():
-			if sorted(list(set(self.queryList)-set(val))) == sorted(self.queryList):
-				similarity.append(0)
+		for doc,val in self.docToken.items():
+			'''
+			if [x for x in self.queryList if x in val] == []:
+				similarity.append("x")
 			else:
+			'''
+			if [x for x in self.queryList if x in val] != []:
 				docLength = 0.0
 				dot = 0.0
-				for y in self.weightHash[doc]:
-					if y != 0: docLength += y**2
-					for z in self.queryWeight.values():
-						dot += z * y
-				similarity.append(round((dot / (math.sqrt(docLength)*math.sqrt(queryLength))),2))
+				for y in self.docWeight[doc]:
+					if y != 0.0:
+						docLength += y**2
+						for z in queryWeight:
+							dot += z * y
+				similarity.append(dot / (math.sqrt(docLength) * math.sqrt(queryLength)))
+			else:
+				similarity.append(0.0)
+		print(list(self.docToken.keys())[similarity.index(max(similarity))])
 		#top 5 retrieval
-		for i in range(4):
+		'''
+		for i in range(1):
 			similarity = list(set(similarity) - set([0.0]))
-			top = similarity.index(sorted(similarity)[-i])
-			print(sorted(similarity)[-i])
+			top = similarity.index(sorted(similarity)[i])
+			print(sorted(similarity)[i])
 			print(top)
 			#print(str(self.rawdocHash.keys()))
 			print(str(i+1) + ": " + self.docHash.keys()[top])
 			print("Title: " + self.rawdocHash.values()[top]["title"][2:])
 			print("Author: " + self.rawdocHash.values()[top]["author"][2:])
+		'''
 
--- Final/search.py
+++ Final/search.py
@@ -6,38 +6,33 @@ import invert
 #->asking for user input
 st = ""
 while st.lower() != "n" and st.lower() != "y":
-	st = raw_input("Would you like to use stopwords?(N/Y)")
+	st = input("Would you like to use stopwords?(N/Y)")
 
 ps = ""
 while ps.lower() != "n" and ps.lower() != "y":
-	ps = raw_input("Would you like to use Porter Stemming Algorithm?(N/Y)")
+	ps = input("Would you like to use Porter Stemming Algorithm?(N/Y)")
 
 
-
-required = raw_input("What are you searching?")
+required = input("What are you searching for?")
 while required != "ZZEND":
-	#the loop will keep running untill the user say ZZEND
-	start = time.time()
-	print("preparing...")
 	tester = invert.main(ps,st)
 	#----------------------------------------
-	query = tester.generateQuery(required)
-	if len(query) == 1:
-		#searching term
-		found = False
-		for key,content in tester.postingHash.items():
-			if required == key:
-				found = True
+	if required != "" and required.lower() != "default":
+		queryWeight = tester.generateQuery(required)
+		if queryWeight == []:
+			print("Invalid Query")
+		elif len(queryWeight) == 1:
+			print("Retrieving Term")
+			if required in tester.postingHash.keys():
 				print("TF = " + str(tester.tokenHash[key]) +".")
 				for doc in content:
-					print("DocID: " + str(doc[0]) + ". DF = " + str(doc[1]))
-		if not found: print("Term: "+ required +"NOT FOUND!\n")
+					print("DocID: " + str(doc[0]) + ". DF = " + str(doc[1]))				
+		else:
+			tester.topKSimilarity(queryWeight)
 	else:
-		#searching sentence
 		tester.retrieveQuery()
+	
 	#----------------------------------------
-	end = time.time()
-	print("The program used: "+ str(end-start)+"seconds.")
-	required = raw_input("What term are you searching for?")
+	required = input("What are you searching for?")
 
 print("Program ends.")
